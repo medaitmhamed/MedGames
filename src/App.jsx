1048d6b57a8332af689f6eb8fe44f0092f1c22a5
Mohamed Ait M'hamed <medaitmhamed1@gmail.com>
2025-08-05 00:16:36 +0100

    correct appp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import PlayModes from "./games/hangman/components/PlayModes";
 import ComingSoon from "./pages/ComingSoon";
 import XOLayout from "./games/tic tac toe/XOLayout";
 import GameModes from "./games/tic tac toe/components/GameModes";
-import TicTacToeBackground from "./games/tic tac toe/components/TicTacToeBackground";
 import OnePlayer from "./games/tic tac toe/components/OnePlayer";
 import TowPlayers from "./games/tic tac toe/components/TowPlayers";
 import Board from "./games/tic tac toe/components/Board";
